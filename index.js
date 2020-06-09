7e85ad0ede5f4fc496cfda348a7ae106c236cc08
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-06-08 21:46:27 -0400

    Button now increments counter on page

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Increment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+    }
+
+    handleClick = () => {
+        // alert('Button clicked');
+        this.setState({
+            value: this.state.value + 1
+        });
+    }
+
+    render = () => {
+        return <div>
+            <button type="button" onClick={this.handleClick}>Increment</button>
+            <h1>{this.state.value}</h1>
+        </div>
+    }
+}
+
+ReactDOM.render(<Increment />, document.getElementById('root'))
